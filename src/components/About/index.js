02877dce3f6df9caaa785aeb17ca650474f5ad69
e1m3m0 <gamateo@gmail.com>
2021-12-15 18:49:32 -0500

    about me componente completed

---
+++ src/components/About/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import profilePicture from "../../assets/images/gmateo.jpg";
+
+function About() {
+  return (
+    <div>
+      <h1>About me</h1>
+      <img
+        src={profilePicture}
+        alt="Guillermo Mateo, Full Stack Web Developer"
+        width="300px"
+        heigh="600px"
+      />
+      <h2>Guillermo Mateo</h2>
+      <h3>Full Stack Web Developer</h3>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. In arcu
+          cursus euismod quis viverra nibh. Gravida cum sociis natoque
+          penatibus. Mi tempus imperdiet nulla malesuada pellentesque elit eget
+          gravida cum. Leo vel fringilla est ullamcorper eget nulla facilisi
+          etiam. Feugiat in ante metus dictum at. Sodales neque sodales ut etiam
+          sit amet nisl purus in. Quam pellentesque nec nam aliquam sem. Est
+          velit egestas dui id ornare arcu odio ut. Placerat in egestas erat
+          imperdiet. Euismod lacinia at quis risus sed vulputate. Magna
+          fermentum iaculis eu non diam phasellus vestibulum lorem. Nisl vel
+          pretium lectus quam id leo in. Nec ultrices dui sapien eget mi proin
+          sed libero. Volutpat diam ut venenatis tellus. Libero justo laoreet
+          sit amet cursus sit amet. Sed augue lacus viverra vitae congue eu
+          consequat ac felis. Maecenas ultricies mi eget mauris. Purus non enim
+          praesent elementum facilisis leo vel fringilla. Nec ullamcorper sit
+          amet risus nullam eget felis. Amet est placerat in egestas erat.
+          <br></br>
+          Suspendisse potenti nullam ac tortor. Semper auctor neque vitae tempus
+          quam pellentesque. Senectus et netus et malesuada fames ac turpis
+          egestas integer. Nec ullamcorper sit amet risus. Fermentum odio eu
+          feugiat pretium nibh ipsum consequat. Auctor elit sed vulputate mi sit
+          amet mauris. Mauris sit amet massa vitae tortor condimentum lacinia.
+          Cursus mattis molestie a iaculis at erat pellentesque. Vel orci porta
+          non pulvinar neque laoreet suspendisse interdum consectetur. Tincidunt
+          eget nullam non nisi est. Ornare suspendisse sed nisi lacus sed
+          viverra tellus. Accumsan tortor posuere ac ut. Sapien faucibus et
+          molestie ac feugiat sed lectus vestibulum mattis. Feugiat nisl pretium
+          fusce id velit ut tortor pretium. Ultricies mi quis hendrerit dolor
+          magna eget est. 
+          <br></br>
+          Rhoncus dolor purus non enim praesent. Non arcu risus
+          quis varius quam quisque id. Semper risus in hendrerit gravida rutrum
+          quisque. Elit duis tristique sollicitudin nibh sit amet commodo nulla
+          facilisi. At varius vel pharetra vel turpis nunc eget lorem dolor. Non
+          blandit massa enim nec dui nunc mattis enim. Massa vitae tortor
+          condimentum lacinia quis vel eros donec ac. Consequat semper viverra
+          nam libero justo laoreet.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
